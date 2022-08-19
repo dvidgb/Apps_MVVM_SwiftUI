c17d6d77350080fe63acfa510da4712cc4ef29ee
David Bueno <davidbuenodbc@gmail.com>
2022-08-19 19:55:22 +0200

    Create project
    Create project in Xcode

---
+++ To_Do_List/To_Do_List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To_Do_List
+//
+//  Created by David Bueno Castro on 19/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ To_Do_List/To_Do_List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To_Do_List
+//
+//  Created by David Bueno Castro on 19/8/22.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
