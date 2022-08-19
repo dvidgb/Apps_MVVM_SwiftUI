c89de4a9eeb843724c985742d3691a133e63166a
David Bueno <davidbuenodbc@gmail.com>
2022-08-19 20:25:08 +0200

    create dataModel
    Create struct that contains model data app

--- To_Do_List/To_Do_List/DataModel/NoteModel.swift
+++ To_Do_List/To_Do_List/DataModel/NoteModel.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct NoteModel: Codable {
+    let id: String
+    var isFavorited: Bool
+    let description: String
+    
+    init(id: String = UUID().uuidString, isFavorited: Bool = false, description: String) {
+        self.id = id
+        self.isFavorited = isFavorited
+        self.description = description
+    }
+}
